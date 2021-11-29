a1c7666eb82e7e0e0350716fe839e3ef65d9edfb
Vladimir.Beglyak <vladimir.beglyak@mail.ru>
2021-11-29 16:57:17 +0300

    Update 29/11/2021

---
+++ src/main/java/by/beglyakdehterenok/store/util/LocalDateConverter.java
@@ -0,0 +1,29 @@
+package by.beglyakdehterenok.store.util;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.Optional;
+
+@Component
+@Converter(autoApply = true)
+public class LocalDateConverter implements AttributeConverter<LocalDate, Date> {
+
+    @Override
+    public Date convertToDatabaseColumn(LocalDate localDate) {
+        return Optional.ofNullable(localDate)
+                .map(Date::valueOf)
+                .orElse(null);
+    }
+
+    @Override
+    public LocalDate convertToEntityAttribute(Date date) {
+        return Optional.ofNullable(date)
+                .map(Date::toLocalDate)
+                .orElse(null);
+    }
+}
